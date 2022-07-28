dc0e4b6e4fd91f71eb236d68d2554282167790a3
michael <michaelglenn88@yahoo.com>
2022-07-28 17:29:57 -0400

    commit tree 761babeededec9ffaa80c8728ac0af89b45c6f8e

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+insert into bears(name,sex) VALUES
+
+ ("Mr. Chocolate", "M"),
+ ("Rowdy", "M"),
+ ("Tabitha", "F"),
+ ("Sergeant Brown", "M"),
+ ("Melissa", "F"),
+ ("Grinch", "M"),
+ ("Wendy", "F"),
+ (null, "F")
